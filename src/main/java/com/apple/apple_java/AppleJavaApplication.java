2f8c55baa2a3f2bf286d398b8f97b3f754ad8e18
Project-DAL <drinkalliquor@gmail.com>
2024-05-23 23:14:08 +0900

    :rocket: first study

---
+++ src/main/java/com/apple/apple_java/AppleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.apple.apple_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleJavaApplication.class, args);
+    }
+
+}
